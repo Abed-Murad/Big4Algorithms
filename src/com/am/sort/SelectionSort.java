3760177a63ba9cb9e90412ecc0f56b4628a5c0ee
Abed Murad <Abdallah.Murad@ProtonMail.com>
2020-03-21 17:56:55 +0200

    Add Selection Search.

--- src/com/am/sort/SelectionSort.java
+++ src/com/am/sort/SelectionSort.java
@@ -1,4 +1,69 @@
 package com.am.sort;
 
+/**
+ * The selection sort algorithm sorts an array by repeatedly finding the minimum element (considering ascending order)
+ * from unsorted part and putting it at the beginning. The algorithm maintains two subarrays in a given array.
+ * <p>
+ * 1) The subarray which is already sorted.
+ * 2) Remaining subarray which is unsorted.
+ * <p>
+ * In every iteration of selection sort, the minimum element (considering ascending order)
+ * from the unsorted subarray is picked and moved to the sorted subarray.
+ * <p>
+ * Following example explains the above steps:
+ * <p>
+ * arr[] = 64 25 12 22 11
+ * <p>
+ * // Find the minimum element in arr[0...4]
+ * // and place it at beginning
+ * 11 25 12 22 64
+ * <p>
+ * // Find the minimum element in arr[1...4]
+ * // and place it at beginning of arr[1...4]
+ * 11 12 25 22 64
+ * <p>
+ * // Find the minimum element in arr[2...4]
+ * // and place it at beginning of arr[2...4]
+ * 11 12 22 25 64
+ * <p>
+ * // Find the minimum element in arr[3...4]
+ * // and place it at beginning of arr[3...4]
+ * 11 12 22 25 64
+ */
 public class SelectionSort {
+
+    private void sort(int[] arr) {
+        int n = arr.length;
+        // One by one move boundary of unsorted subarray
+        for (int i = 0; i < n - 1; i++) {
+            // Find the minimum element in unsorted array
+            int minIndex = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            // Swap the found minimum element with the first element
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+    // Prints the array
+    private void printArray(int[] arr) {
+        int n = arr.length;
+        for (int value : arr) System.out.print(value + " ");
+        System.out.println();
+    }
+
+    // Driver code to test above
+    public static void main(String[] args) {
+        SelectionSort ob = new SelectionSort();
+        int[] arr = {64, 25, 12, 22, 11};
+        ob.sort(arr);
+        System.out.println("Sorted array");
+        ob.printArray(arr);
+    }
+
 }
